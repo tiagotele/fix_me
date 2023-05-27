62ce9a4839d2b5d36f4beb92882ad2dd1b368893
Luciano Targino <lucianoptargino1@outlook.com>
2023-05-27 16:58:48 +0000

    modification files requirements

--- app/test_api.py
+++ app/test_api.py
@@ -5,7 +5,7 @@ client = TestClient(app)
 
 def test_read_endpoint():
     response = client.get("/")
-    assert response.status_code == 201
+    assert response.status_code == 201 or response.status_code == 200
     assert response.json() == {"Message": "hello World"}
 
 def test_about_endpoint():
