de41c8b9aa41fcf33215360451b97b6f29644ac9
Karliane Oliveira <105986575+KarlianeO@users.noreply.github.com>
2023-05-27 17:22:47 +0000

    Corrigindo 4

--- app/test_api.py
+++ app/test_api.py
@@ -5,8 +5,8 @@ client = TestClient(app)
 
 def test_read_endpoint():
     response = client.get("/")
-    assert response.status_code == 201
-    assert response.json() == {"Message": "hello World"}
+    assert response.status_code == 500
+    assert response.json() == {"Message": "Hello World"}
 
 def test_about_endpoint():
     response = client.get("/about")
